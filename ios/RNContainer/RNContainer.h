ae5ca9ed078ca9b2ada3a545cdcbb21411d0eb13
David von Knobelsdorff <david.vonknobelsdorff@dm.de>
2022-08-03 15:52:44 +0200

    use swift

--- ios/RNContainer/RNContainer.h
+++ ios/RNContainer/RNContainer.h
@@ -13,6 +13,4 @@ FOUNDATION_EXPORT double RNContainerVersionNumber;
 //! Project version string for RNContainer.
 FOUNDATION_EXPORT const unsigned char RNContainerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ElectrodeContainer/PublicHeader.h>
-#import <RNContainer/ReactNativeBridge.h>
-#import <RNContainer/ReactNativeViewController.h>
+// In this header, you should import all the public headers of your framework using statements like #import <RNContainer/PublicHeader.h>
